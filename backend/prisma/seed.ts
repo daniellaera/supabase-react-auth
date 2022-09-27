6f890cb69dffa2cc3c3fd01e8984fd72bea9da0c
daniellaera1 <laera.daniel@hotmail.com>
2022-09-27 20:40:54 +0000

    fix db seed bootstrap

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -48,23 +48,10 @@ const userData: Prisma.UserCreateInput[] = [
   },
 ]
 
-async function main() {
-  console.log(`Start seeding ...`)
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
-    })
-    console.log(`Created user with id: ${user.id}`)
-  }
-  console.log(`Seeding finished.`)
+function seedDB() {
+  Promise.all(userData.map(n => prisma.user.create({ data: n })))
+    .then(() => console.info('[SEED] Successfully created user records'))
+    .catch(e => console.error('[SEED] Failed to create user records', e))
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+seedDB();
