9222b15ab2c383a22f85bc006e015e20e97394e0
daniellaera1 <laera.daniel@hotmail.com>
2022-10-08 19:28:55 +0000

    format

--- frontend/src/components/MainNavigation.tsx
+++ frontend/src/components/MainNavigation.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, Flex, HStack, Text, useColorModeValue, VStack } from '@chakra-ui/react';
+import { Avatar, Box, Button, Flex, HStack, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text, useColorModeValue, VStack } from '@chakra-ui/react';
 import { Session } from '@supabase/supabase-js';
 import { useEffect, useState } from 'react';
 import { FiChevronDown, FiLogOut } from 'react-icons/fi';
@@ -11,8 +11,8 @@ import classes from './MainNavigation.module.css';
 const MainNavigation = () => {
   const [session, setSession] = useState<Session | null>();
   const [avatar_url, setAvatarUrl] = useState(null);
-  const [imageUrl, setImageUrl] = useState(null);
-  const [username, setUsername] = useState(null);
+  const [imageUrl, setImageUrl] = useState<string | undefined>();
+  const [username, setUsername] = useState<string | undefined>();
 
   useEffect(() => {
     if (session) getAvatarUrl()
@@ -32,7 +32,7 @@ const MainNavigation = () => {
       }
       const url: any = URL.createObjectURL(data);
       setImageUrl(url);
-      
+
     } catch (error: any) {
       console.log('Error downloading image: ', error.message);
     }
@@ -40,7 +40,7 @@ const MainNavigation = () => {
 
   async function getAvatarUrl() {
     try {
-      
+
       const user = supabaseClient.auth.user();
 
       let { data, error, status } = await supabaseClient
@@ -54,14 +54,14 @@ const MainNavigation = () => {
       }
 
       if (data) {
-        
+
         setAvatarUrl(data.avatar_url);
         setUsername(data.username)
         console.log('avatar uel->>', avatar_url)
       }
     } catch (error: any) {
       alert(error.message);
-    } 
+    }
   }
 
   useEffect(() => {
@@ -72,17 +72,17 @@ const MainNavigation = () => {
     });
   }, []);
 
-    return (
-        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-        
+  return (
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+
         <HStack spacing={8} alignItems={'center'}>
-        <Box >🚀</Box>
-        <HStack
-              as={'nav'}
-              spacing={4}
-              display={{ base: 'none', md: 'flex' }}>
-          
+          <Box >🚀</Box>
+          <HStack
+            as={'nav'}
+            spacing={4}
+            display={{ base: 'none', md: 'flex' }}>
+
             <NavLink
               to="/invoices"
               className={({ isActive }) =>
@@ -92,7 +92,7 @@ const MainNavigation = () => {
             >
               Invoices
             </NavLink>
-          
+
             <NavLink
               to="/posts"
               className={({ isActive }) =>
@@ -102,8 +102,8 @@ const MainNavigation = () => {
             >
               Posts
             </NavLink>
-          
-          
+
+
             <NavLink
               to="/profile"
               className={({ isActive }) =>
@@ -112,47 +112,71 @@ const MainNavigation = () => {
               end
             >
               Profile
-              
+
             </NavLink>
-         
+
           </HStack>
         </HStack>
         <Flex alignItems={'center'}>
-        {session ? (
-          <>
-            <Button
-            onClick={() => supabaseClient.auth.signOut()}
-            variant={'solid'}
-              colorScheme={'teal'}
-              size={'sm'}
-              mr={4}
-              leftIcon={<FiLogOut />}>
-          
-            Logout
-          </Button>
-          {imageUrl &&
-          <HStack>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    imageUrl
-                  }
-                />
-                <VStack
-                  display={{ base: 'none', md: 'flex' }}
-                  alignItems="flex-start"
-                  spacing="1px"
-                  ml="2">
-                  <Text fontSize="sm">{username}</Text>
-                  <Text fontSize="xs" color="gray.600">
-                    Admin
-                  </Text>
-                </VStack>
-                
-              </HStack>
-}
-        </>
-          ): (
+          {session ? (
+            <>
+              <Button
+                onClick={() => supabaseClient.auth.signOut()}
+                variant={'solid'}
+                colorScheme={'teal'}
+                size={'sm'}
+                mr={4}
+                leftIcon={<FiLogOut />}>
+
+                Logout
+              </Button>
+              <Menu>
+                <MenuButton
+                  py={2}
+                  transition="all 0.3s"
+                  _focus={{ boxShadow: 'none' }}>
+                  <HStack>
+                    <Avatar
+                      size={'sm'}
+                      src={
+                        avatar_url ? imageUrl : ''
+                      }
+                    />
+                    <VStack
+                      display={{ base: 'none', md: 'flex' }}
+                      alignItems="flex-start"
+                      spacing="1px"
+                      ml="2">
+                      <Text fontSize="sm">{username}</Text>
+                      <Text fontSize="xs" color="gray.600">
+                        Admin
+                      </Text>
+                    </VStack>
+
+                  </HStack>
+                </MenuButton>
+                <MenuList
+                >
+                  <NavLink
+                    to="/profile"
+                    className={({ isActive }) =>
+                      isActive ? classes.active : undefined
+                    }
+                    end
+                  >
+                    <MenuItem>Profile</MenuItem>
+
+                  </NavLink>
+
+                  <MenuItem>Settings</MenuItem>
+                  <MenuItem>Billing</MenuItem>
+                  <MenuDivider />
+                  <MenuItem onClick={() => supabaseClient.auth.signOut()}>Sign out</MenuItem>
+                </MenuList>
+              </Menu>
+
+            </>
+          ) : (
             <NavLink
               to="/login"
               className={({ isActive }) =>
@@ -161,15 +185,15 @@ const MainNavigation = () => {
               end
             >
               Login
-              
+
             </NavLink>
           )}
-          <ColorModeSwitcher justifySelf="flex-end" marginLeft={4}/>
+          <ColorModeSwitcher justifySelf="flex-end" marginLeft={4} />
         </Flex>
-        
+
       </Flex>
-      </Box>
-    )
+    </Box>
+  )
 }
 
 export default MainNavigation
