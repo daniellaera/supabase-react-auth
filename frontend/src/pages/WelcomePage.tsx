9fd0871f882de420f98cab5878b7d0340f047600
daniellaera <laera.daniel@hotmail.com>
2022-12-04 18:06:15 +0000

    welcome page

--- frontend/src/pages/WelcomePage.tsx
+++ frontend/src/pages/WelcomePage.tsx
@@ -44,7 +44,7 @@ const WelcomePage = () => {
               rounded={'full'}
               color={'white'}
               _hover={{ bg: 'whiteAlpha.500' }}>
-              Explore profiles
+              Publish your profile
             </Button>
           </Stack>
         </Stack>
