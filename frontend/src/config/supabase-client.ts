ae6a9261ee72de8838a4cd62c991025a3578ceaf
daniellaera <laera.daniel@hotmail.com>
2022-12-04 21:49:15 +0000

    config

--- frontend/src/config/supabase-client.ts
+++ frontend/src/config/supabase-client.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+require('dotenv').config();
 
 const supabaseUrl: string = process.env.REACT_APP_SUPABASE_URL!;
 const supabaseAnonKey: string = process.env.REACT_APP_SUPABASE_ANON_KEY!;
--- frontend/src/pages/WelcomePage.tsx
+++ frontend/src/pages/WelcomePage.tsx
@@ -37,7 +37,7 @@ const WelcomePage = () => {
               rounded={'full'}
               color={'white'}
               _hover={{ bg: 'blue.500' }}>
-              Start connecting with other devs
+              Start connecting with others
             </Button>
             <Button
               bg={'whiteAlpha.300'}
