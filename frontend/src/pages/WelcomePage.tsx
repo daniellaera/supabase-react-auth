97ff3e14a076cef5eed0aad3c321c03da357c422
daniellaera <laera.daniel@hotmail.com>
2022-12-05 21:14:45 +0000

    welcome man

--- frontend/src/pages/WelcomePage.tsx
+++ frontend/src/pages/WelcomePage.tsx
@@ -44,7 +44,7 @@ const WelcomePage = () => {
               rounded={'full'}
               color={'white'}
               _hover={{ bg: 'whiteAlpha.500' }}>
-              Explore content
+              Explore contents
             </Button>
           </Stack>
         </Stack>
