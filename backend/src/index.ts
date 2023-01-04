a85ba95502ab022f74dbc8ba0bc0bd569a752fc4
daniellaera <laera.daniel@hotmail.com>
2023-01-04 19:21:13 +0000

    backend port change

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   /* eslint-disable no-console */
   console.log(`Listening: http://localhost:${port}`);
